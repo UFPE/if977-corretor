3626aa8a4dab5be5510bfcd0bf404d45f129de22
Jorge Linhares <jorgehclinhares@gmail.com>
2016-09-28 10:17:51 -0300

    Removendo método get para deploy no Heroic

--- web.js
+++ web.js
@@ -1,10 +1,4 @@
 var express = require('express');
 var app = express();
-
 app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res){
-  res.redirect('/public/index.html');
-});
-
 app.listen(process.env.PORT || 3000);
